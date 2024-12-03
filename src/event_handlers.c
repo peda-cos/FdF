2ea3fddddfd231b5c84b16d72eedab001a674533
Pedro Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-12-03 17:29:57 +0000

    trying again

---
+++ src/event_handlers.c
@@ -0,0 +1,86 @@
+#include "../inc/fdf.h"
+
+void	handle_scroll_event(double offset, double scroll, void *param)
+{
+	t_fdf	*fdf;
+
+	fdf = param;
+	if (offset || mlx_is_key_down(fdf->mlx, MLX_KEY_LEFT_SHIFT)
+		|| mlx_is_key_down(fdf->mlx, MLX_KEY_RIGHT_SHIFT))
+	{
+		fdf->focus += (offset + scroll) * (fdf->focus / 100);
+		fdf->focus = fmin(fmax(fdf->focus, 300), 50000);
+		fdf->camera_height = fdf->focus * 3.3333;
+	}
+	else
+	{
+		scroll = fmin((1500 - fdf->mesh_scale) / 100, scroll);
+		fdf->initial_mesh_scale = fdf->mesh_scale;
+		fdf->mesh_scale += (fdf->mesh_scale / 100) * scroll;
+		fdf->mesh_scale = fmin(fmax(fdf->mesh_scale, 0.01), 1500);
+		fdf->mesh_position.x *= (1 + scroll / 100);
+		fdf->mesh_position.y *= (1 + scroll / 100);
+		if (fdf->vertex_size > 1)
+			fdf->vertex_size *= fdf->mesh_scale / fdf->initial_mesh_scale;
+		fdf->vertex_size = fmax(fdf->vertex_size, 1);
+		if (fdf->dot_size > 1)
+			fdf->dot_size *= fdf->mesh_scale / fdf->initial_mesh_scale;
+		fdf->dot_size = fmax(fdf->dot_size, 1);
+	}
+}
+
+void	handle_mouse_event(enum mouse_key button, enum action action,
+						enum modifier_key modifier, void *param)
+{
+	t_fdf	*fdf;
+
+	fdf = param;
+	if (action == MLX_PRESS && !modifier)
+	{
+		fdf->mouse_buttons_pressed++;
+		if (button == MLX_MOUSE_BUTTON_LEFT && fdf->clicked)
+			fdf->mesh_position = (t_vec2){0, 0};
+		if (button == MLX_MOUSE_BUTTON_LEFT)
+			fdf->clicked = 0.3;
+		mlx_get_mouse_pos(fdf->mlx, &fdf->initial_cursor_position.x,
+			&fdf->initial_cursor_position.y);
+		mlx_set_cursor_mode(fdf->mlx, MLX_MOUSE_HIDDEN);
+	}
+	if (action == MLX_RELEASE && !modifier)
+	{
+		fdf->mouse_buttons_pressed = ft_imax(0, fdf->mouse_buttons_pressed - 1);
+		if (!fdf->mouse_buttons_pressed)
+		{
+			mlx_set_cursor_mode(fdf->mlx, MLX_MOUSE_NORMAL);
+			mlx_set_mouse_pos(fdf->mlx, fdf->initial_cursor_position.x,
+				fdf->initial_cursor_position.y);
+		}
+	}
+}
+
+void	handle_resize_event(int32_t width, int32_t height, void *param)
+{
+	t_fdf	*fdf;
+
+	fdf = param;
+	mlx_resize_image(fdf->depth_map, width, height);
+	if (!fdf->depth_map)
+		clean_exit("mlx_resize_image failed", fdf);
+	mlx_resize_image(fdf->canvas, width, height);
+	if (!fdf->canvas)
+		clean_exit("mlx_resize_image failed", fdf);
+	mlx_resize_image(fdf->background, width, height);
+	if (!fdf->background)
+		clean_exit("mlx_resize_image failed", fdf);
+}
+
+void	initialize_hooks(t_fdf *fdf)
+{
+	mlx_key_hook(fdf->mlx, handle_key_event, fdf);
+	mlx_mouse_hook(fdf->mlx, handle_mouse_event, fdf);
+	mlx_cursor_hook(fdf->mlx, handle_cursor_event, fdf);
+	mlx_scroll_hook(fdf->mlx, handle_scroll_event, fdf);
+	mlx_resize_hook(fdf->mlx, handle_resize_event, fdf);
+	mlx_loop_hook(fdf->mlx, main_loop, fdf);
+	mlx_loop(fdf->mlx);
+}
---
+++ src/map_reader.c
@@ -0,0 +1,96 @@
+#include "../inc/fdf.h"
+
+static void	set_map_dimensions(char *file_path, t_fdf *fdf)
+{
+	int16_t		fd;
+	char		current_char;
+	char		previous_char;
+
+	fd = open(file_path, O_RDONLY);
+	if (fd == -1)
+		clean_exit(file_path, fdf);
+	previous_char = ' ';
+	while (read(fd, &current_char, 1))
+	{
+		if (!ft_isspace(current_char) && ft_isspace(previous_char))
+			fdf->vertex_count++;
+		if (current_char == '\n')
+			fdf->map_height++;
+		if (current_char == '\n' && !fdf->map_width)
+			fdf->map_width = fdf->vertex_count;
+		previous_char = current_char;
+	}
+	if (current_char != '\n')
+		fdf->map_height++;
+	if (!fdf->map_width)
+		fdf->map_width = fdf->vertex_count;
+	close(fd);
+	if (!fdf->vertex_count || fdf->map_width * fdf->map_height != fdf->vertex_count)
+		clean_exit("Invalid map", fdf);
+}
+
+static int32_t	read_next_value(int fd, t_fdf *fdf)
+{
+	int		bytes_read;
+	int		index;
+	char	value_buffer[64];
+
+	bytes_read = 1;
+	index = 0;
+	while (bytes_read && index < 63)
+	{
+		bytes_read = read(fd, &value_buffer[index], 1);
+		if (bytes_read == -1)
+		{
+			close(fd);
+			clean_exit("File read failed", fdf);
+		}
+		if (index && ft_isspace(value_buffer[index]))
+			break ;
+		if (!ft_isspace(value_buffer[index]) && bytes_read)
+			index++;
+	}
+	value_buffer[index] = '\0';
+	validate_value(value_buffer, fdf);
+	return (ft_atoi(value_buffer));
+}
+
+static void	parse_map_data(char *file_path, t_fdf *fdf)
+{
+	int		fd;
+	size_t	vertex_index;
+	size_t	x_position;
+	size_t	y_position;
+
+	fd = open(file_path, O_RDONLY);
+	if (fd == -1)
+		clean_exit(file_path, fdf);
+	vertex_index = 0;
+	x_position = 0;
+	y_position = 0;
+	while (vertex_index < fdf->vertex_count)
+	{
+		fdf->vertices[vertex_index].x = x_position;
+		fdf->vertices[vertex_index].y = y_position;
+		fdf->vertices[vertex_index].z = read_next_value(fd, fdf);
+		vertex_index++;
+		x_position++;
+		if (x_position == fdf->map_width)
+		{
+			y_position++;
+			x_position = 0;
+		}
+	}
+	close(fd);
+}
+
+void	initialize_fdf(char *file_path, t_fdf *fdf)
+{
+	set_map_dimensions(file_path, fdf);
+	fdf->vertices = (t_vertex *)malloc(fdf->vertex_count * sizeof(t_vertex));
+	if (!fdf->vertices)
+		clean_exit("Memory allocation error", fdf);
+	parse_map_data(file_path, fdf);
+	set_vertex_colors(fdf);
+	fdf->height_scale = 1;
+}
